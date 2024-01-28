31f91bae36784e87b842e66ea2f12f59ce223858
sabs <dziedziochs@gmail.com>
2024-01-28 19:23:35 +0100

    Multiple files handling. Limit number of files and filesize

--- src/components/ImageOptimizer.js
+++ src/components/ImageOptimizer.js
@@ -1,47 +1,81 @@
 import React, { useState } from "react";
 
 const ImageOptimizer = () => {
-  const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedFiles, setSelectedFiles] = useState([]);
   const [selectedFormat, setSelectedFormat] = useState("png");
-  const [optimizedDataURL, setOptimizedDataURL] = useState(null);
   const [isOptimizing, setIsOptimizing] = useState(false);
-  const [unsupportedFiles, setUnsupportedFiles] = useState([]);
+  const [optimizedDataURLs, setOptimizedDataURLs] = useState([]);
+  const [uploadErrorMessage, setUploadErrorMessage] = useState(null);
+  const [originalFileSizes, setOriginalFileSizes] = useState([]);
 
-  //Check if the file type is supported
+  const bytesToMB = (bytes) => {
+    const megabytes = bytes / (1024 * 1024);
+    return megabytes.toFixed(2) + "MB";
+  };
+
   const isFileTypeUnsupported = (file) => {
     const unsupportedTypes = ["image/svg+xml", "image/tiff", "image/tif"];
     return unsupportedTypes.includes(file.type);
   };
 
-  // Handles the change event when a user selects a file. Checks if the file type is unsupported and updates state accordingly.
   const handleFileChange = (event) => {
-    const file = event.target.files[0];
+    const files = event.target.files;
+    const newSelectedFiles = [];
 
-    if (isFileTypeUnsupported(file)) {
-      setUnsupportedFiles([file.name]);
-      setSelectedFile(null);
-      setOptimizedDataURL(null);
-    } else {
-      setSelectedFile(file);
-      setUnsupportedFiles([]);
+    setUploadErrorMessage(null);
+
+    if (selectedFiles.length + files.length > 5) {
+      setUploadErrorMessage("Too many files. Please upload up to 5 files.");
+      return;
     }
+
+    for (const file of files) {
+      // Check if the file type is unsupported
+      if (isFileTypeUnsupported(file)) {
+        setUploadErrorMessage(
+          `Unsupported file type. Can't optimize ${
+            file.type.match(/\/([^+/]+)/)[1]
+          }`
+        );
+      } else {
+        const maxSizeInMB = 2.2;
+        const maxSizeInBytes = maxSizeInMB * 1024 * 1024; // 5MB
+        if (file.size > maxSizeInBytes) {
+          setUploadErrorMessage(
+            `File size exceeds the limit. Please upload files up to ${maxSizeInMB}MB.`
+          );
+        } else {
+          newSelectedFiles.push(file);
+        }
+      }
+    }
+    setSelectedFiles((prevSelectedFiles) => [
+      ...prevSelectedFiles,
+      ...newSelectedFiles,
+    ]);
   };
 
   const handleFormatChange = (event) => {
     setSelectedFormat(event.target.value);
   };
 
-  // Initiates the optimization process if a file is selected, setting the optimization flag to true and calling the optimizeImage function.
-  // Displays a console message if no file is selected.
   const handleOptimize = () => {
-    if (selectedFile) {
+    if (selectedFiles.length > 0) {
       setIsOptimizing(true);
-      optimizeImage(selectedFile, selectedFormat);
+      setOriginalFileSizes([]);
+
+      // Optimize each selected file
+      for (const file of selectedFiles) {
+        setOriginalFileSizes((prevOriginalFileSizes) => [
+          ...prevOriginalFileSizes,
+          file.size,
+        ]);
+        optimizeImage(file, selectedFormat);
+      }
     } else {
-      console.log("No file selected.");
+      setUploadErrorMessage("No files selected.");
     }
   };
-
   const optimizeImage = (file, format) => {
     const reader = new FileReader();
 
@@ -53,7 +87,6 @@ const ImageOptimizer = () => {
         const canvas = document.createElement("canvas");
         const ctx = canvas.getContext("2d");
 
-        // Resize the image (adjust dimensions as needed)
         const maxWidth = 800;
         const maxHeight = 600;
         let newWidth = img.width;
@@ -72,17 +105,18 @@ const ImageOptimizer = () => {
         canvas.width = newWidth;
         canvas.height = newHeight;
 
-        // Draw the image on the canvas
         ctx.drawImage(img, 0, 0, newWidth, newHeight);
 
-        // Convert the canvas content to a data URL with the selected format
         const optimizedDataURL =
           format === "jpg"
-            ? canvas.toDataURL("image/jpeg", 0.8) // Adjust quality as needed
+            ? canvas.toDataURL("image/jpeg", 0.8)
             : canvas.toDataURL("image/png");
 
-        // Set the optimized data URL in state
-        setOptimizedDataURL(optimizedDataURL);
+        setOptimizedDataURLs((prevOptimizedDataURLs) => [
+          ...prevOptimizedDataURLs,
+          optimizedDataURL,
+        ]);
+
         setIsOptimizing(false);
       };
     };
@@ -91,10 +125,14 @@ const ImageOptimizer = () => {
   };
 
   const handleDrop = (event) => {
-    if (!selectedFile) {
-      event.preventDefault();
-      const file = event.dataTransfer.files[0];
-      setSelectedFile(file);
+    event.preventDefault();
+    const files = event.dataTransfer.files;
+    const newSelectedFiles = [];
+
+    for (const file of files) {
+      if (!isFileTypeUnsupported(file)) {
+        newSelectedFiles.push(file);
+      }
     }
   };
 
@@ -103,12 +141,10 @@ const ImageOptimizer = () => {
   };
 
   const handleSelectImageClick = () => {
-    // Trigger click on the hidden file input
-    if (!selectedFile) {
+    if (selectedFiles.length === 0) {
       fileInputRef.current.click();
     }
   };
-
   const fileInputRef = React.createRef();
 
   const getSanitizedFileName = (fileName) => {
@@ -120,27 +156,41 @@ const ImageOptimizer = () => {
       .replace(/^-+|-+$/g, "");
   };
 
-  const handleDownload = () => {
-    if (optimizedDataURL) {
+  const handleDownload = (index) => {
+    if (optimizedDataURLs[index]) {
       const downloadLink = document.createElement("a");
       const fileExtension = selectedFormat === "jpg" ? "jpeg" : selectedFormat;
-      const sanitizedFileName = getSanitizedFileName(selectedFile.name);
+
+      const sanitizedFileName = getSanitizedFileName(selectedFiles[index].name);
       const yubiFileName = `yubi-${sanitizedFileName}.${fileExtension}`;
 
       downloadLink.download = yubiFileName;
-      downloadLink.href = optimizedDataURL;
+      downloadLink.href = optimizedDataURLs[index];
       document.body.appendChild(downloadLink);
       downloadLink.click();
       document.body.removeChild(downloadLink);
     } else {
-      console.log("No optimized image available for download.");
+      console.log(
+        `No optimized image available for download: Image ${index + 1}`
+      );
+    }
+  };
+
+  const handleDownloadAll = () => {
+    if (optimizedDataURLs.length > 0) {
+      for (let i = 0; i < optimizedDataURLs.length; i++) {
+        handleDownload(i);
+      }
+    } else {
+      console.log("No optimized images available for download.");
     }
   };
 
   const handleChooseDifferentFile = (event) => {
     event.preventDefault();
-    setSelectedFile(null);
-    setOptimizedDataURL(null);
+    setSelectedFiles([]);
+    setOptimizedDataURLs([]);
+    setUploadErrorMessage(null);
   };
 
   return (
@@ -158,15 +208,23 @@ const ImageOptimizer = () => {
         }}
         onClick={handleSelectImageClick}
       >
-        {selectedFile ? (
+        {uploadErrorMessage && (
+          <p style={{ color: "red" }}>{uploadErrorMessage}</p>
+        )}
+        {selectedFiles.length > 0 ? (
           <div>
-            <p>Selected File: {selectedFile.name}</p>
+            <p>Selected Files:</p>
+            <ul>
+              {selectedFiles.map((file, index) => (
+                <li key={index}>{file.name}</li>
+              ))}
+            </ul>
             <button onClick={handleChooseDifferentFile}>
-              Choose Different File
+              Choose Different Files
             </button>
           </div>
         ) : (
-          <p>Drag and drop an image here or click to select one.</p>
+          <p>Drag and drop images here or click to select them.</p>
         )}
         <input
           type="file"
@@ -174,20 +232,10 @@ const ImageOptimizer = () => {
           accept="image/*"
           style={{ display: "none" }}
           ref={fileInputRef}
+          multiple
         />
       </div>
-      {unsupportedFiles.length > 0 && (
-        <div>
-          <p>Unsupported Files:</p>
-          <ul>
-            {unsupportedFiles.map((fileName) => (
-              <li key={fileName}>{fileName}</li>
-            ))}
-          </ul>
-          <p>Please choose a different file.</p>
-        </div>
-      )}
-      {selectedFile && (
+      {selectedFiles.length > 0 && (
         <div>
           <label>
             Output Format:
@@ -197,23 +245,36 @@ const ImageOptimizer = () => {
             </select>
           </label>
           <button onClick={handleOptimize} disabled={isOptimizing}>
-            Optimize Image
+            Optimize Images
           </button>
           {isOptimizing && (
-            <p>Optimizing image... (show loading icon or progress bar)</p>
+            <p>Optimizing images... (show loading icon or progress bar)</p>
           )}
-          {optimizedDataURL && !isOptimizing && (
+          {optimizedDataURLs.length > 0 && !isOptimizing && (
             <div>
-              <p>Image Optimized!</p>
-              <img
-                src={optimizedDataURL}
-                alt="Optimized"
-                style={{ maxWidth: "100%" }}
-              />
-
-              <button onClick={handleDownload} disabled={!optimizedDataURL}>
-                Download Optimized Image
-              </button>
+              <p>Images Optimized!</p>
+              {optimizedDataURLs.map((optimizedDataURL, index) => (
+                <div key={index}>
+                  <img
+                    src={optimizedDataURL}
+                    alt={`Optimized ${selectedFiles[index].name}`}
+                    style={{ maxWidth: "10%" }}
+                  />
+                  <p>
+                    Original Size: {bytesToMB(originalFileSizes[index])} | New
+                    Size: {bytesToMB(optimizedDataURL.length)}
+                  </p>
+                  <button
+                    onClick={() => handleDownload(index)}
+                    disabled={!optimizedDataURL}
+                  >
+                    Download Optimized Image
+                  </button>
+                </div>
+              ))}
+              {optimizedDataURLs.length > 1 && (
+                <button onClick={handleDownloadAll}>Download All Images</button>
+              )}
             </div>
           )}
         </div>
@@ -221,5 +282,4 @@ const ImageOptimizer = () => {
     </div>
   );
 };
-
 export default ImageOptimizer;
